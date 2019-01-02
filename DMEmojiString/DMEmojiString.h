98425a6ef0b0b8c5fb73ed501f764fa4983a5285
Desmond McNamee <desmondmcnamee@gmail.com>
2019-01-02 22:46:59 +0100

    Initial Commit

---
+++ DMEmojiString/DMEmojiString.h
@@ -0,0 +1,19 @@
+//
+//  DMEmojiString.h
+//  DMEmojiString
+//
+//  Created by Desmond McNamee on 02.01.19.
+//  Copyright © 2019 Desmond McNamee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DMEmojiString.
+FOUNDATION_EXPORT double DMEmojiStringVersionNumber;
+
+//! Project version string for DMEmojiString.
+FOUNDATION_EXPORT const unsigned char DMEmojiStringVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMEmojiString/PublicHeader.h>
+
+
